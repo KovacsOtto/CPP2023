e47bf06d5cf071a9305b7f888829bf38f22702b7
KovacsOtto <kovacs.otto@student.ms.sapientia.ro>
2023-09-25 15:33:42 +0300

    elsofeladat

--- lab01/main_01.cpp
+++ lab01/main_01.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int countBits(int number);
+bool setBit(int& number, int order);
 
 int main() {
-    std::cout << "Hello, Lab_01" << std::endl;
+    for(int i=0;i<=127;i++){
+        cout << countBits(i) << endl;
+    }
     return 0;
+}
+
+
+int countBits(int number){
+    if(!number | 0) return 0;
+    if(number & 1) return 1+ countBits(number>>1);
+    return countBits(number>>1);
+}
+bool setBit(int& number, int order){
+
 }
